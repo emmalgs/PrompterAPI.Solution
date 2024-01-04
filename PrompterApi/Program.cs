a5c51a12277a4625814a3b8774ec94dbafd06304
Emma Gerigscott <elgerig@gmail.com>
2024-01-04 09:36:52 -0800

    intialize program.cs file

---
+++ PrompterApi/Program.cs
@@ -0,0 +1,28 @@
+using PrompterApi.Models;
+using PrompterApi.Startup;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.RegisterServices();
+
+builder.Services.AddDbContext<PrompterApiContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"],
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
+
+var app = builder.Build();
+
+app.ConfigureSwagger();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
+
+app.Run();
