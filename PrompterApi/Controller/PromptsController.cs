7384a160c4aa6cacd69da207669eb49c032cdd33
Emma Gerigscott <elgerig@gmail.com>
2024-01-04 11:47:50 -0800

    Refactor PromptsController to include Get and Post methods

--- PrompterApi/Controller/PromptsController.cs
+++ PrompterApi/Controller/PromptsController.cs
@@ -2,22 +2,30 @@ using PrompterApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 
-// namespace PrompterApi.AddControllers
-// {
-//   [ApiController]
-//   public class PromptsController: ControllerBase
-//   {
-//   //   private PrompterApiContext _db;
+namespace PrompterApi.AddControllers
+{
+  [ApiController]
+  public class PromptsController: ControllerBase
+  {
+    private readonly PrompterApiContext _db;
+    public PromptsController(PrompterApiContext db)
+    {
+      _db = db;
+    }
 
-//   //   public PromptsController(PrompterApiContext db)
-//   //   {
-//   //     _db = db;
-//   //   }
+    [HttpGet]
+    public async Task<string> Get()
+    {
+      Prompt prompt = await _db.Prompts.OrderByDescending(p => p.PromptId).FirstOrDefaultAsync();
+      return prompt.PromptText;
+    }
 
-//   //   [HttpGet]
-//   //   public ActionResult<Prompt> Get()
-//   //   {
-//   //     return Ok(_db.Prompt);
-//   //   }
-//   // }
-// }
+    [HttpPost]
+    public async Task<ActionResult> Post(Prompt prompt)
+    {
+      _db.Prompts.Add(prompt);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(Get), new { id = prompt.PromptId }, prompt);
+    }
+  }
+}
