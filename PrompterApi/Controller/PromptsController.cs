9a55508408c00e2742d953105bf5679981b27408
Emma Gerigscott <elgerig@gmail.com>
2024-01-04 09:38:13 -0800

    add basic controller file

---
+++ PrompterApi/Controller/PromptsController.cs
@@ -0,0 +1,23 @@
+using PrompterApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+// namespace PrompterApi.AddControllers
+// {
+//   [ApiController]
+//   public class PromptsController: ControllerBase
+//   {
+//   //   private PrompterApiContext _db;
+
+//   //   public PromptsController(PrompterApiContext db)
+//   //   {
+//   //     _db = db;
+//   //   }
+
+//   //   [HttpGet]
+//   //   public ActionResult<Prompt> Get()
+//   //   {
+//   //     return Ok(_db.Prompt);
+//   //   }
+//   // }
+// }
