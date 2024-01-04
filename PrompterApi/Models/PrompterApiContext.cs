ef5287b5d288631855febb94e5fd1dd8ec5e3784
Emma Gerigscott <elgerig@gmail.com>
2024-01-04 09:37:31 -0800

    add Prompt db context file

---
+++ PrompterApi/Models/PrompterApiContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PrompterApi.Models
+{
+  public class PrompterApiContext : DbContext
+  {
+    public DbSet<Prompt> Prompts { get; set; }
+
+    public PrompterApiContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    builder.Entity<Prompt>()
+      .HasData(
+        new Prompt { PromptId = 1, PromptText = "What is your favorite color?" }
+      );
+  }
+  }
+}
