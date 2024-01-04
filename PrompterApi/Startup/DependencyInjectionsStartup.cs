74eb3b0ce09a5a64bd876cf2414c632106a53d2a
Emma Gerigscott <elgerig@gmail.com>
2024-01-04 09:37:55 -0800

    add dependency injection for program.cs

---
+++ PrompterApi/Startup/DependencyInjectionsStartup.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc.Versioning;
+using System.Text;
+
+namespace PrompterApi.Startup;
+
+public static class DependencyInjectionSetup
+{
+  public static IServiceCollection RegisterServices(this IServiceCollection services)
+  {
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+
+    services.AddControllers().AddNewtonsoftJson();
+
+    services.AddApiVersioning(opt =>
+                                    {
+                                      opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+                                      opt.AssumeDefaultVersionWhenUnspecified = true;
+                                      opt.ReportApiVersions = true;
+                                      opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                      new HeaderApiVersionReader("x-api-version"),
+                                                                                      new MediaTypeApiVersionReader("x-api-version"));
+                                    });
+
+    services.AddVersionedApiExplorer(setup =>
+                                      {
+                                          setup.GroupNameFormat = "'v'VVV";
+                                          setup.SubstituteApiVersionInUrl = true;
+                                      });
+
+    services.ConfigureOptions<SwaggerConfiguration.ConfigureSwaggerOptions>();
+
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+
+    return services;
+  }
+}
