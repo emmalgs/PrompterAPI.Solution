f345d6f1bbce2dec10888fafb60d1e85c55abdb9
Emma Gerigscott <elgerig@gmail.com>
2024-01-05 12:13:21 -0800

    Refactor program.cs file

--- PrompterApi/Program.cs
+++ PrompterApi/Program.cs
@@ -14,19 +14,8 @@ builder.Services.AddDbContext<PrompterApiContext>(
                     )
                   )
                 );
-builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
-    app.UseHttpsRedirection();
-}
 
 app.ConfigureSwagger();
 
