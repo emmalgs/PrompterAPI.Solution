1c9e3150f4b46aba0e5791aeb794b576b93a4409
Emma Gerigscott <elgerig@gmail.com>
2024-01-05 10:28:34 -0800

    Add GetAll and Delete methods to PromptsController

--- PrompterApi/Controller/PromptsController.cs
+++ PrompterApi/Controller/PromptsController.cs
@@ -22,10 +22,26 @@ namespace PrompterApi.AddControllers
       return text;
     }
 
+    [HttpGet]
+    public async Task<List<Prompt>> GetAll()
+    {
+      List<Prompt> prompts = await _db.Prompts.ToListAsync();
+      return prompts;
+    }
+
     [HttpPost]
     public async Task<ActionResult> Post(Prompt prompt)
     {
       _db.Prompts.Add(prompt);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+      Prompt prompt = await _db.Prompts.FirstOrDefaultAsync(p => p.PromptId == id);
+      _db.Prompts.Remove(prompt);
       await _db.SaveChangesAsync();
       return NoContent();
     }
